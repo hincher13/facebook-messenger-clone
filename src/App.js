6ef56326c1d9ad837d2dc0531f593f3f0893f4b4
Ray Hinch <rahinch@icloud.com>
2021-05-15 17:23:07 -0400

    Added input, button, and firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+
+  console.log(input)
+
   return (
     <div className="App">
       <h1>Hello Clever Programmers!</h1>
+
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button>Send Message</button>
+      {/* Input field */}
+      {/* button */}
+      {/* messages */}
     </div>
   );
 }
